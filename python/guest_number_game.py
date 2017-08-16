c79322ac05d68ae17f21dac0dc15be0f8f9487d3
bassoom <330421442@qq.com>
2017-08-17 06:08:56 +0800

    [ADD]1，语境按照要求来更改，不可以假设语境。2，修正部分逻辑错误。

--- python/guest_number_game.py
+++ python/guest_number_game.py
@@ -2,27 +2,27 @@
  
 guesses_made = 0
  
-name = raw_input('小朋友你好，快来和我一起玩猜数字游戏吧.请输入输入你的英文名并按回车键开始游戏\n')
+name = raw_input('你好，欢迎跟我一起玩猜数字游戏。请输入你的名字开始吧：\n')
  
 number = random.randint(1, 20)
-print '{0}小朋友, 我现在想好了一个1到20之间的整数数字，你有三次机会来猜.前两次如果你没猜对我会告诉你你猜的数字是大了还是小了，你可以根据我的提示来继续猜，明白了吗？'.format(name)
+print '你好，{0}, 欢迎来跟我玩猜数字的游戏。我已近在1-20中选择了一个数字，你有三次机会来猜中我选的数字。每次猜测的时候，如果没猜对，我会告诉你猜测的数字是大还是小，你可以根据我的提示来进行下一次测试。明白了吗？'.format(name)
  
 while guesses_made < 3:
  
-    guess = int(raw_input('请输入你猜的数字并按回车键：'))
+    guess = int(raw_input('请输入你猜的数字：'))
  
     guesses_made += 1
  
     if guess < number:
-        print '你猜的数字比我想的有点小，请继续猜：'
+        print '你猜的数字比我选择的有点小：'
  
     if guess > number:
-        print '你猜的数字比我想的有点大，请继续猜：'
+        print '你猜的数字比我选择的有点大：'
  
     if guess == number:
         break
  
 if guess == number:
-    print '哇, 恭喜你！只用了 {1} 次机会就猜对了，难道你就是未来的数学小天才吗？小的我佩服，佩服!'.format(name, guesses_made)
+    print '恭喜你！只用了 {1} 次机会就猜对了。'.format(name, guesses_made)
 else:
-    print '不好意思，机会用完了. 其实我心里想的数字是 {0}'.format(number)
+    print '不好意思，机会用光了。其实我选择的数字是 {0}'.format(number)
